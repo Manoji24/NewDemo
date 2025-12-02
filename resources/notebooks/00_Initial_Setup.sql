baee901e8a9261529cd3a83ea47d6a289b68d11a
mmanojipec@gmail.com <mmanojipec@gmail.com>
2025-12-02 08:35:33 +0000

    s3 as external volume

--- resources/notebooks/00_Initial_Setup.sql
+++ resources/notebooks/00_Initial_Setup.sql
@@ -9,11 +9,11 @@
 
 -- COMMAND ----------
 
-CREATE CATALOG IF NOT EXISTS CAPSTONE;
+CREATE CATALOG IF NOT EXISTS IDENTIFIER(:catalog)
 
 -- COMMAND ----------
 
-USE CATALOG CAPSTONE;
+USE CATALOG IDENTIFIER(:catalog);
 
 -- COMMAND ----------
 
@@ -37,58 +37,59 @@ CREATE SCHEMA IF NOT EXISTS CAPSTONE.GOLD;
 -- MAGIC - Provide cross account access using Trustrelation
 -- MAGIC - Followed by running the following script
 -- MAGIC
--- MAGIC _CREATE STORAGE CREDENTIAL s3_iam_role_cred
--- MAGIC WITH IAM_ROLE 'arn:aws:iam::123456789012:role/DatabricksS3AccessRole'
--- MAGIC COMMENT 'Credential for accessing S3 via IAM role';_
+-- MAGIC _databricks storage-credentials create --json '{\"name\": \"sc_external_s3_capstone\", \"aws_iam_role\": {\"role_arn\": \"arn for iam role\"}}'_
 -- MAGIC
--- MAGIC _CREATE EXTERNAL LOCATION CAPSTONE_BRONZE_RAW
--- MAGIC URL 's3://CAPSTONE/'
--- MAGIC WITH STORAGE CREDENTIAL s3_iam_role_cred
--- MAGIC COMMENT 'CAPSTONE data stored in S3';_
+-- MAGIC _CREATE EXTERNAL LOCATION EXT_S3_CAPSTONE URL 's3://capstone-proj-src/'
+-- MAGIC      WITH (CREDENTIAL sc_external_s3_capstone)
+-- MAGIC      COMMENT 'Capstone Src data path in S3';_
 
 -- COMMAND ----------
 
-CREATE VOLUME IF NOT EXISTS CAPSTONE.BRONZE.RAW;
+-- CREATE EXTERNAL LOCATION EXT_S3_CAPSTONE URL 's3://capstone-proj-src/' 
+-- WITH STORAGE CREDENTIAL sc_external_s3_capstone
+-- -- COMMENT 'CAPSTONE data stored in S3'
+
+CREATE EXTERNAL LOCATION IF NOT EXISTS EXT_S3_CAPSTONE URL 's3://capstone-proj-src/'
+     WITH (CREDENTIAL sc_external_s3_capstone)
+     COMMENT 'Capstone Src data path in S3';
+
+-- COMMAND ----------
+
+
+
+CREATE EXTERNAL VOLUME IF NOT EXISTS CAPSTONE.BRONZE.RAW
+        COMMENT 'This is the external volume for source data'
+        LOCATION 's3://capstone-proj-src/'
+
+-- COMMAND ----------
+
+LIST '/Volumes/capstone/bronze/raw'
 
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC
--- MAGIC import os
+-- MAGIC def create_directory_ifnotexists(file_path):
+-- MAGIC   try:
+-- MAGIC     dbutils.fs.ls(file_path)
+-- MAGIC   except Exception as e:
+-- MAGIC     if "java.io.FileNotFoundException" in str(e):
+-- MAGIC         dbutils.fs.mkdirs(file_path)
+-- MAGIC     else:
+-- MAGIC         raise e
+
+-- COMMAND ----------
+
+-- MAGIC %python
 -- MAGIC
 -- MAGIC base_raw_path = "/Volumes/CAPSTONE/bronze/raw/files/landing"               # root landing zone
 -- MAGIC transactions_path = f"{base_raw_path}/transactions_raw"
 -- MAGIC customers_path = f"{base_raw_path}/customers_raw"
 -- MAGIC products_path = f"{base_raw_path}/products_raw"
 -- MAGIC
--- MAGIC dbutils.fs.mkdirs(base_raw_path)
--- MAGIC dbutils.fs.mkdirs("/Volumes/CAPSTONE/bronze/raw/files/checkpoint")
--- MAGIC dbutils.fs.mkdirs("/Volumes/CAPSTONE/bronze/raw/files/schema")
--- MAGIC dbutils.fs.mkdirs(transactions_path)
--- MAGIC dbutils.fs.mkdirs(customers_path)
--- MAGIC dbutils.fs.mkdirs(products_path)
--- MAGIC
--- MAGIC
-
--- COMMAND ----------
-
--- MAGIC %md
--- MAGIC ### Uploading Files:
--- MAGIC
--- MAGIC Files are uploaded to user folder. They are then copied to the volume.
-
--- COMMAND ----------
-
--- MAGIC %python
--- MAGIC dbutils.fs.cp(f"{os.getcwd()}/products_raw.csv",products_path)
--- MAGIC dbutils.fs.cp(f"{os.getcwd()}/customer_raw.csv",customers_path)
--- MAGIC dbutils.fs.cp(f"{os.getcwd()}/transaction_raw.csv",transactions_path)
-
--- COMMAND ----------
-
--- MAGIC %skip
--- MAGIC %python
--- MAGIC dbutils.fs.cp(f"{os.getcwd()}/products_raw.csv",products_path + "/products_raw1.csv")
+-- MAGIC create_directory_ifnotexists(base_raw_path)
+-- MAGIC create_directory_ifnotexists(transactions_path)
+-- MAGIC create_directory_ifnotexists(customers_path)
+-- MAGIC create_directory_ifnotexists(products_path)
 
 -- COMMAND ----------
 
